ce43bd92b1ab2b3484c175d56772fc26358d8801
Johannes Jahn <johannes.jahn@outlook.com>
2022-08-20 22:37:50 +0200

    Remove not needed endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,6 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiCreatedResponse } from '@nestjs/swagger';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { UserResponseDTO } from './dtos/user.dto';
 
 @Controller()
 export class AppController {
@@ -11,17 +8,4 @@ export class AppController {
     private readonly appService: AppService,
     private authService: AuthService,
   ) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @ApiBearerAuth()
-  @ApiCreatedResponse({ type: UserResponseDTO })
-  @UseGuards(JwtAuthGuard)
-  @Get('/test')
-  getAnyUser(@Request() req): Promise<UserResponseDTO> {
-    return this.appService.getUser(req.user);
-  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,20 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { User } from './users/user.entity';
-import { UsersService } from './users/users.service';
 
 @Injectable()
-export class AppService {
-  constructor(private usersService: UsersService) {}
-
-  getHello(): string {
-    return 'Testing the deployment';
-  }
-
-  getFirstUser(): Promise<User> {
-    return this.usersService.findAny();
-  }
-
-  async getUser(user: any) {
-    return await this.usersService.findOne(user.userId);
-  }
-}
+export class AppService {}
