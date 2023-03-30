022c368d985547ceb8b5395c46ac893778fa1068
Johannes Jahn <johannes.jahn@outlook.com>
2023-03-30 15:26:37 +0200

    Fix debug endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiBearerAuth } from '@nestjs/swagger';
 import { AppGateway } from './app.gateway';
 
 @Controller()
 export class AppController {
   constructor(private appGateway: AppGateway) {}
 
-  @ApiBearerAuth()
   @Get('/debug')
   async getDebug() {
     this.appGateway.sendToAll('Hello World');
