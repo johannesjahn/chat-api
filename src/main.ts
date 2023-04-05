132d5ba6d8a73fab9270c3056132662131c13fbb
Johannes Jahn <johannes.jahn@outlook.com>
2023-04-05 21:05:42 +0200

    Version Bump

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Chat - API')
     .setDescription('Chat - API Description')
-    .setVersion('0.0.23')
+    .setVersion('0.0.24')
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
