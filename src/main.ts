6b2441dfb59cf60d1dd85a202eb4c9e7db73f263
Johannes Jahn <johannes.jahn@futurice.com>
2023-04-11 14:04:27 +0200

    Version Bump

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Chat - API')
     .setDescription('Chat - API Description')
-    .setVersion('0.0.26')
+    .setVersion('0.0.27')
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
