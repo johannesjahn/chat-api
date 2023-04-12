339b4d06f8004c92a2b046b768fa3c33fbd3cb8c
Johannes Jahn <johannes.jahn@futurice.com>
2023-04-12 12:34:14 +0200

    Add status page

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,48 @@ import { PostModule } from './post/post.module';
 import { UsersModule } from './users/users.module';
 import { AwsService } from './aws/aws.service';
 import { AppGateway } from './app.gateway';
+import { StatusMonitorModule } from 'nestjs-status-monitor';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: `${process.env.ENV_PATH ?? '.env'}` }),
+    StatusMonitorModule.forRoot({
+      title: 'Nacho Chat Status', // Default title
+      path: '/status',
+      socketPath: '/status-socket.io', // In case you use a custom path
+      port: null, // Defaults to NestJS port
+      spans: [
+        {
+          interval: 1, // Every second
+          retention: 60, // Keep 60 datapoints in memory
+        },
+        {
+          interval: 5, // Every 5 seconds
+          retention: 60,
+        },
+        {
+          interval: 15, // Every 15 seconds
+          retention: 60,
+        },
+      ],
+      chartVisibility: {
+        cpu: true,
+        mem: true,
+        load: true,
+        responseTime: true,
+        rps: true,
+        statusCodes: true,
+      },
+      ignoreStartsWith: ['/admin'], // paths to ignore for responseTime stats
+      healthChecks: [
+        {
+          protocol: 'http',
+          host: 'localhost',
+          path: '/app/debug',
+          port: 3000,
+        },
+      ],
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: `${process.env.DATABASE_URL}`,
