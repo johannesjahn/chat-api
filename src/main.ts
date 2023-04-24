ecca940f3735a81cc6f09034758f7a3994c73b49
Johannes Jahn <johannes.jahn@futurice.com>
2023-04-24 13:07:56 +0200

    Fix version display

--- src/main.ts
+++ src/main.ts
@@ -11,12 +11,12 @@ async function bootstrap() {
   app.setGlobalPrefix('/app');
 
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  // const pjson = require('../package.json');
+  const pjson = require('../package.json');
 
   const config = new DocumentBuilder()
     .setTitle('Chat - API')
     .setDescription('Chat - API Description')
-    .setVersion('pjson.version')
+    .setVersion(pjson.version)
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
