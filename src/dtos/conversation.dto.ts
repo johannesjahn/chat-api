79f8f51a627e2abdcf1c1a8ff679b164ed495d3a
Johannes Jahn <johannes.jahn@futurice.com>
2023-04-24 13:15:46 +0200

    Fix create conversation dto type

--- src/dtos/conversation.dto.ts
+++ src/dtos/conversation.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateConversationRequestDTO {
-  @ApiProperty()
+  @ApiProperty({ type: 'array', items: { type: 'number' } })
   partnerIds: number[];
 }
