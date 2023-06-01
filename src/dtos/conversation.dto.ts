ed757bd55a393bf82d2ae736a1c33ebd04334e96
Johannes Jahn <johannes.jahn@outlook.com>
2023-06-01 16:54:47 +0200

    Add conversation dto doc

--- src/dtos/conversation.dto.ts
+++ src/dtos/conversation.dto.ts
@@ -1,6 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateConversationRequestDTO {
-  @ApiProperty({ type: Number, isArray: true })
+  @ApiProperty({
+    type: Number,
+    isArray: true,
+    description:
+      "The partner ids of a conversation (Can't be empty or contain self)",
+  })
   partnerIds: number[];
 }
