2034dae5d115e474930a16dd9d57aea96b4a0480
Johannes Jahn <johannes.jahn@outlook.com>
2023-06-08 00:41:40 +0200

    Update change password doc

--- src/dtos/changePassword.dto.ts
+++ src/dtos/changePassword.dto.ts
@@ -4,6 +4,8 @@ export class ChangePasswordDTO {
   @ApiProperty({ description: 'The new password' })
   password: string;
 
-  @ApiProperty({ description: 'The new password again' })
+  @ApiProperty({
+    description: 'The new password again, to confirm it got typed in properly',
+  })
   passwordConfirm: string;
 }
