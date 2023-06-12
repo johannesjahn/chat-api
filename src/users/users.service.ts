0bb167a8a9c73d45d35ed60ba7b9375c34e7a36f
Johannes Jahn <johannes.jahn@outlook.com>
2023-06-12 14:31:44 +0200

    Remove unused code from users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,10 +10,19 @@ export class UsersService {
     private usersRepository: Repository<User>,
   ) {}
 
+  /**
+   * Find all users
+   * @returns All users
+   */
   findAll(): Promise<User[]> {
     return this.usersRepository.find();
   }
 
+  /**
+   * Find all users except the user with the given id
+   * @param userId The id of the user to exclude
+   * @returns All users except the user with the given id
+   */
   async findAllWithoutSelf(userId: number): Promise<User[]> {
     return this.usersRepository
       .createQueryBuilder()
@@ -21,27 +30,24 @@ export class UsersService {
       .getMany();
   }
 
+  /**
+   * Find one user by id
+   * @param id The id of the user to find
+   * @returns The user with the given id
+   */
   findOne(id: number): Promise<User> {
     return this.usersRepository.findOne({ where: { id: id } });
   }
 
+  /**
+   * Find user by username
+   * @param username THe username of the user to find
+   * @returns The user with the given username
+   */
   async findUser(username: string): Promise<User> {
     const result = await this.usersRepository.find({
       where: { username: username },
     });
     return result[0];
-  }
-
-  async findAny(): Promise<User> {
-    const user = await this.usersRepository.find({ take: 1 });
-    return user.length > 0 ? user[0] : null;
-  }
-
-  async insert(user: User): Promise<void> {
-    await this.usersRepository.save(user);
-  }
-
-  async remove(id: string): Promise<void> {
-    await this.usersRepository.delete(id);
   }
 }
