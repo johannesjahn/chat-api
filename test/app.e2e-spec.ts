7e26fecfb4203e401f68e1f170d923def133756e
Johannes Jahn <johannes.jahn@outlook.com>
2023-07-06 11:46:13 +0200

    use tabs instead of spaces

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,34 +4,34 @@ import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+	let app: INestApplication;
 
-  beforeAll(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+	beforeAll(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [AppModule],
+		}).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+		app = moduleFixture.createNestApplication();
+		await app.init();
+	});
 
-  afterAll(async () => {
-    await app.close();
-  });
+	afterAll(async () => {
+		await app.close();
+	});
 
-  it('/ (GET)', async () => {
-    return await request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+	it('/ (GET)', async () => {
+		return await request(app.getHttpServer())
+			.get('/')
+			.expect(200)
+			.expect('Hello World!');
+	});
 
-  it('/auth/login (POST', async () => {
-    const result = await request(app.getHttpServer())
-      .post('/auth/login')
-      .send({ username: 'Nachobar', password: '12345678' });
+	it('/auth/login (POST', async () => {
+		const result = await request(app.getHttpServer())
+			.post('/auth/login')
+			.send({ username: 'Nachobar', password: '12345678' });
 
-    expect(result.body.access_token).toBeDefined();
-    expect(result.statusCode).toBe(201);
-  });
+		expect(result.body.access_token).toBeDefined();
+		expect(result.statusCode).toBe(201);
+	});
 });
