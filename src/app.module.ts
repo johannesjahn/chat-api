1b24482f695297e8c0627c785f87f43b1e1212d8
Johannes Jahn <johannes.jahn@outlook.com>
2024-02-16 09:26:59 +0100

    Remove aws sdk

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,6 @@ import { ChatModule } from './chat/chat.module';
 import { AppLoggerMiddleware } from './middleware/app.logger.middleware';
 import { PostModule } from './post/post.module';
 import { UsersModule } from './users/users.module';
-import { AwsService } from './aws/aws.service';
 import { AppGateway } from './app.gateway';
 import { StatusMonitorModule } from 'nestjs-status-monitor';
 
@@ -68,7 +67,7 @@ import { StatusMonitorModule } from 'nestjs-status-monitor';
 		PostModule,
 	],
 	controllers: [AppController],
-	providers: [AppService, AwsService, AppGateway],
+	providers: [AppService, AppGateway],
 })
 export class AppModule {
 	configure(consumer: MiddlewareConsumer): void {
--- src/aws/aws.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import * as AWS from 'aws-sdk';
-
-@Injectable()
-export class AwsService {
-	s3 = new AWS.S3({});
-}
