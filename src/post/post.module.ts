9f7fb7b7e779cc892ff4b189e9d0301873223bad
Johannes Jahn <johannes.jahn@outlook.com>
2024-06-17 16:19:30 +0200

    fix

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -4,9 +4,10 @@ import { PostController } from './post.controller';
 import { Comment, Post, Reply } from './post.entity';
 import { PostGateway } from './post.gateway';
 import { PostService } from './post.service';
+import { User } from '../users/user.entity';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Post, Comment, Reply])],
+	imports: [TypeOrmModule.forFeature([Post, Comment, Reply, User])],
 	controllers: [PostController],
 	providers: [PostService, PostGateway],
 	exports: [PostGateway],
