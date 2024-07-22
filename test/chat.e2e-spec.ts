fbb899cfc229886c65dfb034cecc2acf8eacb37a
Johannes Jahn <johannes.jahn@outlook.com>
2024-07-22 22:50:17 +0200

    add e2e test

--- test/chat.e2e-spec.ts
+++ test/chat.e2e-spec.ts
@@ -72,4 +72,48 @@ describe('AppController (e2e)', () => {
 		expect(firstResponse.body as ConversationResponseDTO[]).toHaveLength(1);
 		expect(secondResponse.body as ConversationResponseDTO[]).toHaveLength(1);
 	});
+
+	it('app/chat/create-conversation (POST)', async () => {
+		const firstCredentials = await createAccount();
+		const secondCredentials = await createAccount();
+		const thirdCredentials = await createAccount();
+
+		const createConversationRequestDTO: CreateConversationRequestDTO = {
+			partnerIds: [secondCredentials.id, thirdCredentials.id],
+		};
+
+		await request(app.getHttpServer())
+			.post('/chat/create-conversation')
+			.set('Authorization', `Bearer ${firstCredentials.accessToken}`)
+			.send(createConversationRequestDTO);
+
+		const firstResponse = await request(app.getHttpServer())
+			.get('/chat/get-conversations')
+			.set('Authorization', `Bearer ${firstCredentials.accessToken}`)
+			.send();
+
+		const secondResponse = await request(app.getHttpServer())
+			.get('/chat/get-conversations')
+			.set('Authorization', `Bearer ${secondCredentials.accessToken}`)
+			.send();
+
+		const thirdResponse = await request(app.getHttpServer())
+			.get('/chat/get-conversations')
+			.set('Authorization', `Bearer ${thirdCredentials.accessToken}`)
+			.send();
+
+		expect(firstResponse.body as ConversationResponseDTO[]).toHaveLength(1);
+		expect(secondResponse.body as ConversationResponseDTO[]).toHaveLength(1);
+		expect(thirdResponse.body as ConversationResponseDTO[]).toHaveLength(1);
+
+		expect(
+			(firstResponse.body as ConversationResponseDTO[])[0].participants,
+		).toHaveLength(3);
+		expect(
+			(secondResponse.body as ConversationResponseDTO[])[0].participants,
+		).toHaveLength(3);
+		expect(
+			(thirdResponse.body as ConversationResponseDTO[])[0].participants,
+		).toHaveLength(3);
+	});
 });
