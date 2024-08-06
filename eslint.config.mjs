408f5e86d0e2a2d941cc62c318ebd8b50c4d27a7
Johannes Jahn <johannes.jahn@outlook.com>
2024-08-06 11:18:31 +0200

    migrate lint

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+	{
+		ignores: ['node_modules', 'dist', 'build', 'out', 'coverage'],
+	},
+	{ files: ['**/*.{js,mjs,cjs,ts}'] },
+	{ languageOptions: { globals: globals.browser } },
+	// extends prettier
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	eslintConfigPrettier,
+	{ rules: { '@typescript-eslint/no-explicit-any': 'off' } },
+];
