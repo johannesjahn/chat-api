c02894b69a55d6418fe4f13b59e48b056b82f363
Johannes Jahn <johannes.jahn@outlook.com>
2024-08-06 11:18:31 +0200

    migrate lint

--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -75,7 +75,6 @@ describe('UserService', () => {
 		const allUsers = await service.findAll();
 		const theUser = await service.findOne(allUsers[0].id);
 		expect(theUser).not.toBeNull();
-		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 		expect(theUser!.id).toBe(allUsers[0].id);
 		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 		expect(theUser!.username).toBe(allUsers[0].username);
